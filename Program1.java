b8e450af09e5be1b27d41ba4eb2919fb77e24710
Xingyvo <159099828+Xingyvo@users.noreply.github.com>
2024-03-05 19:23:23 -0500

    Pasted a code for generating random integers.

--- Program1.java
+++ Program1.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 class Program1 extends ConsoleProgram {
 
   /**
@@ -6,8 +8,15 @@ class Program1 extends ConsoleProgram {
   */
   
   public void run() {
-    // your code goes here
     
-    
+       // create a random object
+       Random myRandom = new Random();
+
+
+       // generate a random int number and store it in a variable
+       // this generates a number between 10 and 30
+       int intRandom = myRandom.nextInt((30 - 10) + 1) + 10;      
+       System.out.println(intRandom);
+
   }
 }
